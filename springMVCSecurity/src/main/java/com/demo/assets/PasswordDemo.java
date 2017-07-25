345c7deff458a1cf8bf835175e599b2a4363b492
cguevaraaguilar <cguevara_aguilar@hotmail.com>
2017-07-24 22:12:34 -0500

    md5

---
+++ springMVCSecurity/src/main/java/com/demo/assets/PasswordDemo.java
@@ -0,0 +1,20 @@
+package com.demo.assets;
+
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordDemo {
+
+	public String getMD5Hash (String plainText) {
+		
+		Md5PasswordEncoder encoder = new Md5PasswordEncoder ();
+		
+		String encrypted = encoder.encodePassword(plainText, null);
+		
+		System.out.println("Password encrypted usind MD5 = " + encrypted);
+		
+		return (encrypted);
+	} // public String getMD5Hash (String plainText) {
+	
+} // public class PasswordDemo {
---
+++ springMVCSecurity/src/main/java/com/demo/console/AppDemo.java
@@ -0,0 +1,14 @@
+package com.demo.console;
+
+import com.demo.assets.PasswordDemo;
+
+public class AppDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PasswordDemo pwd = new PasswordDemo ();
+		
+		pwd.getMD5Hash("1234567");
+	} // public static void main(String[] args) {
+} // public class AppDemo {
